622fe033704b94e0f6d2435993178bac2b9db699
EmmanuelloDaniele <143413061+EmmanuelloDaniele@users.noreply.github.com>
2023-08-30 14:28:52 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Liron</span>
+            Ciao, sono <span className="text-[#915EFF]">Daniele</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />
