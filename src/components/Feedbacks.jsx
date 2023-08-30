6f010d7fdb31d1ac573d9e071f343ec53113853e
EmmanuelloDaniele <143413061+EmmanuelloDaniele@users.noreply.github.com>
2023-08-30 18:15:35 +0200

    Update Feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -49,7 +49,7 @@ const Feedbacks = () => {
         className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
       >
         <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What others say</p>
+          <p className={styles.sectionSubText}>Cosa dicono su di me</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
