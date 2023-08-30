516eea716f97ba2a6c9bf1ccb75853d5b2da305e
EmmanuelloDaniele <143413061+EmmanuelloDaniele@users.noreply.github.com>
2023-08-30 14:30:58 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Ciao, sono <span className="text-[#915EFF]">Daniele</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
-            interfaces and web applications
+            Dottore in Ingegneria dell'innovazione per le imprese digitali <br className="sm:block hidden" />
+            Curriculum Informatico
           </p>
         </div>
       </div>
