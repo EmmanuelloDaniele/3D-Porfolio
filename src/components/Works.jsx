8178bebdd5b4521f7fad432091e10d9e42620c4e
EmmanuelloDaniele <143413061+EmmanuelloDaniele@users.noreply.github.com>
2023-08-30 18:20:06 +0200

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -69,11 +69,12 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Seguendo diversi progetti ho avuto la possibilità di migliorare le mie 
+          abilità e la mia esperienza nel mondo reale del lavoro. Ogni progetto è 
+          personalizzato in base alle richieste del cliente e risolve delle difficoltà 
+          semplicizando con delle soluzioni costum su misura. Questi lavori riflettono 
+          le mie abilità a risolvere problemi, lavorare con differenti tipi di tecnologie 
+          e gestire effettivamente un progetto.
         </motion.p>
       </div>
 
